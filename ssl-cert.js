9570671986d5a9b2b4efa1a66c8e5ed5a3e92a24
mwmaleks <mwmaleks@gmx.com>
2013-12-21 22:21:31 +0400

    [#2] fixed cert -> crt, added conditions for to verify each provided certificate

--- ssl-cert.js
+++ ssl-cert.js
@@ -21,7 +21,7 @@ var Ssl = function() {
     var args = arguments ? arguments[0] : '';
     var opt = {
         ca_key  : ( args && args.ca_key )  ? args.ca_key  : 'ca.key',
-        ca_cert : ( args && args.ca_cert ) ? args.ca_cert : 'ca.crt',
+        ca_crt : ( args && args.ca_crt )   ? args.ca_crt : 'ca.crt',
         ca_pwd  : ( args && args.ca_pwd )  ? args.ca_pwd  : 'ca.pwd',
         srv_key : ( args && args.srv_key ) ? args.srv_key : 'server.key',
         srv_crt : ( args && args.srv_crt ) ? args.srv_crt : 'server.crt',
@@ -41,20 +41,21 @@ var Ssl = function() {
 
     //    check if ssl folder exists
     ( !fs.existsSync( path.join(rootDir, 'ssl' ) ) ) && fs.mkdirSync( path.join( rootDir, 'ssl') );
-    
+
     var   dfd_ca_key  = new _.Deferred()
-        , dfd_ca_cert = new _.Deferred()
+        , dfd_ca_crt  = new _.Deferred()
         , dfd_srv_key = new _.Deferred()
         , dfd_srv_crt = new _.Deferred()
-        , dfd_no_pass = new _.Deferred();
+        , dfd_no_pass = new _.Deferred()
+        , dfd_create_all = new _.Deferred();
 
     var _this = this;
 
     fs.exists( path.join( rootDir, opt.ca_key ), function(ex) {
         dfd_ca_key.resolve(ex);
     });
-    fs.exists( path.join( rootDir, opt.ca_cert ), function(ex) {
-        dfd_ca_cert.resolve(ex);
+    fs.exists( path.join( rootDir, opt.ca_crt ), function(ex) {
+        dfd_ca_crt.resolve(ex);
     });
     fs.exists( path.join( rootDir, opt.srv_key ), function(ex) {
         dfd_srv_key.resolve(ex);
@@ -62,15 +63,49 @@ var Ssl = function() {
     fs.exists( path.join( rootDir, opt.srv_crt ), function(ex) {
         dfd_srv_crt.resolve(ex);
     });
-    _.when( dfd_ca_key, dfd_ca_cert, dfd_srv_key,dfd_srv_crt ).done( function( ca_key, ca_crt, srv_key, srv_crt ) {
+    _.when( dfd_ca_key, dfd_ca_crt, dfd_srv_key,dfd_srv_crt ).done( function( ca_key, ca_crt, srv_key, srv_crt ) {
 
         if ( ca_key && ca_crt && srv_key && srv_crt )  {
 
+//          verify CA certificate
+            verify.ca( opt.ca_crt ).done( function(valid_ca) {
+
+//              if CA ok verify server certificate
+                if ( valid_ca ) {
+                    verify.srv( opt.ca_crt, opt.srv_crt ).done( function(valid_srv) {
+
+//                  if server certificate is ok create no password certificate
+                        if ( valid_srv ) {
+                            dfd_no_pass.resolve();
+                            return;
+                        }
+                        console.log('provided server certificate is invalid');
+                        dfd_create_all.resolve();
+
+                    }).fail( function(err) {
+                            dfd_create_all.reject(err);
+                    });
+                    return;
+                }
+                console.log('provided CA is invalid');
+                dfd_create_all.resolve();
+
+            }).fail( function(err) {
+                    dfd_create_all.reject(err);
+            });
 
         } else if ( !( ca_key || ca_crt || srv_key || srv_crt ) ) {
 
+// in case of there is no one file we create all
+            dfd_create_all.resolve();
 
+        } else {
+//          some of the specified files not exist
+            throw ' one or more specified files do not exist'
         }
-        throw ' one or more specified files do not exist'
+        dfd_create_all.done( function() {
+            
+        }).fail();
+
     });
 };
