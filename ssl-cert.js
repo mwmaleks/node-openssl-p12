fda5bf409f6e0bebac4a33934adee22d1bcac56b
mwmaleks <mwmaleks@gmx.com>
2013-12-21 21:44:22 +0400

    [#2] created ssl-cert.js file, added initial requires, create function Ssl - the constructor of ssl object, added arguments processing

--- ssl-cert.js
+++ ssl-cert.js
@@ -1,3 +1,31 @@
 /**
  * Created by mwmaleks on 21.12.13.
  */
+
+var   spawn =   require('child_process').spawn
+    , _ =       require('underscore')
+    , fs =      require('fs')
+//    , os =      require('os')
+    , rootDir = process.cwd()
+    , path =    require('path')
+    , Guid = require('guid');
+
+_.mixin( require('underscore.deferred') );
+
+function genPwd() {
+    return Guid.create().value();
+}
+
+var Ssl = function() {
+
+    var args = arguments ? arguments[0] : '';
+    var opt = {
+        ca_key  : ( args && args.ca_key )  ? args.ca_key  : 'ca.key',
+        ca_cert : ( args && args.ca_cert ) ? args.ca_cert : 'ca.crt',
+        ca_pwd  : ( args && args.ca_pwd )  ? args.ca_pwd  : 'ca.pwd',
+        srv_key : ( args && args.srv_key ) ? args.srv_key : 'server.key',
+        srv_crt : ( args && args.srv_crt ) ? args.srv_crt : 'server.crt',
+        srv_pwd : ( args && args.srv_pwd ) ? args.srv_pwd : 'server.pwd'
+    };
+
+};
