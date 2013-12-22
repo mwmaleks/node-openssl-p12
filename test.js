d8cd15a10029ea8d1d001d8a3789c0f3cc2cd743
mwmaleks <mwmaleks@gmx.com>
2013-12-22 13:54:16 +0400

    [#3] created test.js

--- test.js
+++ test.js
@@ -1,3 +1,36 @@
 /**
  * Created by mwmaleks on 21.12.13.
  */
+
+var   _ = require('underscore')
+    , Ssl_cert = require('./ssl-cert.js').Ssl;
+
+if ( _.isNull(Ssl_cert) )
+    throw 'Class Position in not defined';
+
+if ( !_.isFunction(Ssl_cert) )
+    throw 'Varible Ssl is not Class (function constructor)';
+
+
+var ssl_options = {
+           country: 'RU',
+           state: 'Krasnodar',
+           city: 'Krasnosar',
+           company: 'Pilotbook',
+           division: 'Pilotbook',
+           domain :'pilotbook.ru',
+           email: 'admin@pilotbook.ru'
+};
+
+
+var ssl = new Ssl_cert( ssl_options).done( function(_ssl) {
+
+    console.log('done!');
+
+}).fail( function(err) {
+
+    console.log('error: ', err);
+});
+
+
+
