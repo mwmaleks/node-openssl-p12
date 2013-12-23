3c88835437d338846df82077630a3c7b79c4426b
pavelpower <pavel.power@gmail.com>
2013-12-23 22:04:16 +0400

    sample used function then with deferred

--- test/test_deferred.js
+++ test/test_deferred.js
@@ -1,8 +1,42 @@
 var _ =       require('underscore.deferred');
 
 
-var a = new _.Deferred(),
-    b = new _.Deferred();
+function getA(a) {
+    var deferred = _.Deferred();
+    console.log('a', a, a + 1);
+    setTimeout(function() {
+        deferred.resolve(a + 1);
+    },100);
+    return deferred;
+}
 
-console.log(a);
-console.log(b);
+function getB(b) {
+    var deferred = _.Deferred();
+    console.log('b', b, b + 3);
+    setTimeout(function() {
+        deferred.resolve(b + 3);
+    }, 200);
+    return deferred;
+}
+
+function getC(c) {
+    var deferred = _.Deferred();
+    console.log('c', c, c + 5);
+    setTimeout(function() {
+        deferred.resolve(c + 5);
+    }, 100);
+    return deferred;
+}
+
+
+
+var dfd = _.Deferred();
+   dfd.then(null, getA)
+       .then(getB)
+       .then(getC)
+       .then(function(v) {
+           console.log(v);
+       })
+   ;
+
+dfd.reject(1);
