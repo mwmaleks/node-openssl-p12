b3ca75d57062bc9c950951e36da2b3c9ca2cf0af
mwmaleks <mwmaleks@gmx.com>
2013-12-23 18:29:08 +0400

    [#3] test for createClientSSL

--- test/test.js
+++ test/test.js
@@ -2,63 +2,25 @@
  * Created by mwmaleks on 21.12.13.
  */
 
-var   _                   = require('underscore')
-    , createClientKey     = require('../lib/clients-ssl').createClientKey
-    , createCSR           = require('../lib/clients-ssl').createCSR
-    , createClientCRT     = require('../lib/clients-ssl').createClientCRT
-    , createPKS12         = require('../lib/clients-ssl').createPKS12
-    , getFingerprint      = require('../lib/clients-ssl').getFingerprint;
-
+var createClientSSL     = require('../lib/clients-ssl').createClientSSL;
 
 var options = {
-    clientFileName :'client000',
-    C:'RU',
-    ST: 'Krasnodar',
-    L: 'Krasnodar',
-    O: 'PilotBook',
-    OU: 'PilotBook',
-    CN: 'Konstantin',
-    emailAddress: 'admin@pilotbook.ru',
-    clientPass: '1905',
+    clientFileName :'client001',
+    C:'EX',
+    ST: 'Example',
+    L: 'Example',
+    O: 'Example',
+    OU: 'Example',
+    CN: 'Example',
+    emailAddress: 'example@example.ex',
+    clientPass: 'q1w2e3r4t5y6',
     caFileName: 'ca',
-    serial: '02',
+    serial: '01',
     days: 365
 };
 
-createClientKey(options).done( function(arg) {
-
-    console.log('createClientKey: '.green, 'Done!'.yellow);
-
-    createCSR(options).done( function() {
-
-        console.log('createCSR: '.green, 'Done!'.yellow);
-
-        createClientCRT(options).done( function() {
-
-            console.log('createClientCRT: '.green, 'Done!'.yellow);
-
-            createPKS12(options).done( function() {
-
-                console.log('createPKS12: '.green, 'Done!'.yellow);
-
-                getFingerprint(options).done( function() {
-                    console.log('arguments = '.green, arguments);
-                    console.log( getFingerprint.fingerprint, ' - Done!'.yellow );
-                }).fail(function(err) {
-                        console.log('error: '.red, err);
-                });
-            }).fail(function(err) {
-                console.log('error: '.red, err);
-            });
-        }).fail(function(err) {
-            console.log('error: '.red, err);
-        });
-    }).fail(function(err) {
-            console.log('error: '.red, err);
-    });
+createClientSSL(options).done( function(fingerprint) {
+    console.log('client\'s ', options.clientFileName, ' .key, .csr, .crt, .p12 created. SHA-1 fingerprint is:', fingerprint );
 }).fail( function(err) {
-   console.log('error: '.red, err);
+    console.log(err);
 });
-
-
-
