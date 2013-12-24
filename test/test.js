5332322bc9421571b9854f0bd98b9b2bc1fef18f
mwmaleks <mwmaleks@gmx.com>
2013-12-24 21:29:20 +0400

    fixed usless params, fixed _fingerprint console.log

--- test/test.js
+++ test/test.js
@@ -5,31 +5,29 @@
 var createClientSSL     = require('../lib/clients-ssl').createClientSSL;
 
 var options = {
-    clientFileName :'client001'
-//    C:'EX',
-//    ST: 'Example',
-//    L: 'Example',
-//    O: 'Example',
-//    OU: 'Example',
-//    CN: 'Example',
-//    emailAddress: 'example@example.ex',
-//    clientPass: 'q1w2e3r4t5y6',
-//    caFileName: 'ca',
-//    serial: '01',
-//    days: 365
+    clientFileName :'client001',
+    C:'EX',
+    ST: 'Example',
+    L: 'Example',
+    O: 'Example',
+    OU: 'Example',
+    CN: 'Example',
+    emailAddress: 'example@example.ex',
+    clientPass: 'q1w2e3r4t5y6',
+    caFileName: 'ca',
+    serial: '01',
+    days: 365
 };
 
-createClientSSL(options).then( function(params, options, stdout, stderr) {
+createClientSSL(options).then( function(options, stdout, stderr) {
     console.log('-------------');
 
-    if ( params.indexOf('-fingerprint') !== -1 ) {
-        var _fingerprint = stdout.match(/Fingerprint=([0-9a-fA-F:]+)$/m);
+    var _fingerprint = stdout.match(/Fingerprint=([0-9a-fA-F:]+)$/m);
 
-        if ( _fingerprint ) {
-            console.log(' *.key, *.csr, *.crt, *.p12 created. SHA-1 fingerprint is:', _fingerprint );
-        } else {
-            console.error('No fingerprint got');
-        }
+    if ( _fingerprint ) {
+        console.log(' *.key, *.csr, *.crt, *.p12 created. SHA-1 fingerprint is:', _fingerprint[1] );
+    } else {
+        console.error('No fingerprint got');
     }
 
     console.log('createCRT:', options );
