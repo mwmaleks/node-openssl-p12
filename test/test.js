932bbd1295d1e71b9c5fd3d5db7ed4af2a3c8130
mwmaleks <mwmaleks@gmx.com>
2013-12-24 12:50:56 +0400

    [#4] removed unused code, _path global, getOptions set defaults, added deferred when instead of bothDone, fixed files exist logic, added example password less ca

--- test/test.js
+++ test/test.js
@@ -5,23 +5,23 @@
 var createClientSSL     = require('../lib/clients-ssl').createClientSSL;
 
 var options = {
-    clientFileName :'client001',
-    C:'EX',
-    ST: 'Example',
-    L: 'Example',
-    O: 'Example',
-    OU: 'Example',
-    CN: 'Example',
-    emailAddress: 'example@example.ex',
-    clientPass: 'q1w2e3r4t5y6',
-    caFileName: 'ca',
-    serial: '01',
-    days: 365
+    clientFileName :'client001'
+//    C:'EX',
+//    ST: 'Example',
+//    L: 'Example',
+//    O: 'Example',
+//    OU: 'Example',
+//    CN: 'Example',
+//    emailAddress: 'example@example.ex',
+//    clientPass: 'q1w2e3r4t5y6',
+//    caFileName: 'ca',
+//    serial: '01',
+//    days: 365
 };
 
 createClientSSL(options).done( function(fingerprint) {
     console.log('-------------');
-    console.log('client\'s ', options.clientFileName, ' .key, .csr, .crt, .p12 created. SHA-1 fingerprint is:', fingerprint );
+    console.log( options.clientFileName, ' *.key, *.csr, *.crt, *.p12 created. SHA-1 fingerprint is:', fingerprint );
 }).fail( function(err) {
     console.log('-------------');
     console.log('ERORS: ', err);
