cda54b88ff6b5aff830cd1783311b39e93c41ea7
mwmaleks <mwmaleks@gmx.com>
2014-02-06 19:13:52 +0400

    fixed test

--- test/test.js
+++ test/test.js
@@ -20,11 +20,14 @@ var options = {
 };
 
 createClientSSL(options).done(function(options, fingerprint) {
+
     console.log('-------------');
-    console.log('  *.key, *.csr, *.crt, *.p12 created. SHA-1 fingerprint is:', fingerprint);
+    console.log('Client\'s certificate created. SHA-1 fingerprint is:', fingerprint);
     console.log('createCRT:', options);
     console.log('-------------');
+
 }).fail( function(err) {
+
     console.log('-------------');
-    console.log('ERORS: ', err);
+    console.log('ERRORS: ', err);
 });
