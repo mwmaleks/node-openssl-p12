62e7dc30ef23ccd8a61a09dc130906bd20a15ad1
mwmaleks <mwmaleks@gmx.com>
2014-02-07 19:47:35 +0400

    added public key as example

--- test/test.js
+++ test/test.js
@@ -16,7 +16,8 @@ var options = {
     clientPass: 'q1w2e3r4t5y6',
     caFileName: 'ca',
     serial: '01',
-    days: 365
+    days: 365,
+    publicKey: 'MIICQDCCASgwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCmBRkLLlYoSLHC9U3zCmoLvDxmIvYBX8PfXLYIcmzo0zKRf/HfOGJiALAfmqWHStLJTJ+igWAGE6H7ZLbpv6STbFOfPSf8lBZX8IsLufHyvh4LLX5sfm5Fpg30tsknlrG8B0eAJQXwuyyoXlRB58SSNHtEA13qkXUDB7GxZ53f76LuuqEngYHgsigRcW/7KBU41Qv0LiwhFf4mRsGnoATLJIP2lzMVzfksYLvfzSTirUlFu1p1TlEOaqqEi/YL2A7KHGhcOmlSEeDCQK+PsrrUFq3Pckw7vLgaWiFVBWf4hgxCus9iJXAcP7vR7zYxEqAXIVwwABpQu69KYt076w+LAgMBAAEWADANBgkqhkiG9w0BAQQFAAOCAQEAY8P4TRfCMDanooBuSgaZXV02Q38rnb27gm2NKHwf6iJ6qeb8k1hzHS0O3FEK82fmg1+EPtXswR3UQR87pYuJ3JeS/qiLYsPeGqPoNSmMZjqtnCqWRwl6OomkJLJoIa1oPuIViPDCM39C0JOVabyQNYMqL45dl47fLyPuiEPcJ7i9TeoT2CnCse645km1yRfE1vGB/BYK+rhlKcCUUpYHb/Nqcl+Ry3YiRv0q1hUjLWsEsYE6/dTLusqmsUPnRH586S4yWtJbO7xuGv0PgPNaUBlJIK8liEpbFrcCWwHyncRz6vY2hlpw6p5FdPM/HNeNOGpRUpGa4j6D72Lww9TPtw=='
 };
 
 createClientSSL(options).done(function(options, fingerprint) {
