63605788cdd831dbcaf9ff052418b542a70fb346
mwmaleks <mwmaleks@gmx.com>
2014-02-07 19:57:06 +0400

    fixed module name in index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
  * Created by mwmaleks on 24.12.13.
  */
 
-exports.createClientSSL = require('./lib/openssl-p12').createClientSSL;
+exports.createClientSSL = require('./lib/node-openssl-p12').createClientSSL;
